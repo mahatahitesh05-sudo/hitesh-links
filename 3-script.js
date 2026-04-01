55b29dfd78c54f5df4c10b7869ea15fcac018429
mahatahitesh05-sudo <mahatahitesh05@gmail.com>
2026-04-01 12:53:02 +0530

    Update full website

--- 3-script.js
+++ 3-script.js
@@ -1,3 +1,4 @@
+// IMAGE MODAL
 function openModal(img){
 document.getElementById("imgModal").style.display="block";
 document.getElementById("fullImg").src=img.src;
@@ -6,3 +7,46 @@ document.getElementById("fullImg").src=img.src;
 function closeModal(){
 document.getElementById("imgModal").style.display="none";
 }
+
+
+// TYPING EFFECT
+const texts = ["Student", "Tech Learner", "Video Editor", "Graphic Designer"];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type(){
+
+if(count === texts.length){
+count = 0;
+}
+
+currentText = texts[count];
+letter = currentText.slice(0, ++index);
+
+document.getElementById("typing").textContent = letter;
+
+if(letter.length === currentText.length){
+count++;
+index = 0;
+setTimeout(type, 1000);
+}else{
+setTimeout(type, 100);
+}
+
+})();
+
+
+// SMOOTH SCROLL
+document.querySelectorAll('nav a').forEach(anchor => {
+anchor.addEventListener('click', function(e) {
+e.preventDefault();
+
+document.querySelector(this.getAttribute('href')).scrollIntoView({
+behavior: 'smooth'
+});
+
+});
+});
